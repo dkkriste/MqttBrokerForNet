b42a1cc43a85eadc167d3991e0e21ee1f3fd1806
David Kristensen <dkkriste@ifi.uio.no>
2017-02-16 23:03:38 +0100

    made more managers singleton

--- Broker/Program.cs
+++ Broker/Program.cs
@@ -1,27 +1,25 @@
-﻿using System.IO;
-using Microsoft.Extensions.Configuration;
-using MqttBrokerForNet.Business;
-using MqttBrokerForNet.Business.Managers;
-using MqttBrokerForNet.Business.Workers;
-using MqttBrokerForNet.Domain.Contracts.Managers;
-using MqttBrokerForNet.Domain.Contracts.Workers;
-using MqttBrokerForNet.Domain.Entities.Configuration;
-
-namespace MqttBrokerForNet.Broker
+﻿namespace MqttBrokerForNet.Broker
 {
     using System;
+    using System.IO;
     using System.Linq;
     using System.Threading;
 
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Options;
 
+    using MqttBrokerForNet.Business;
     using MqttBrokerForNet.Business.Factories;
     using MqttBrokerForNet.Business.Handlers;
+    using MqttBrokerForNet.Business.Managers;
     using MqttBrokerForNet.Business.Network;
+    using MqttBrokerForNet.Business.Workers;
     using MqttBrokerForNet.Domain.Contracts.Factories;
     using MqttBrokerForNet.Domain.Contracts.Handlers;
+    using MqttBrokerForNet.Domain.Contracts.Managers;
     using MqttBrokerForNet.Domain.Contracts.Network;
+    using MqttBrokerForNet.Domain.Contracts.Workers;
+    using MqttBrokerForNet.Domain.Entities.Configuration;
 
     public class Program
     {
@@ -47,7 +45,6 @@ namespace MqttBrokerForNet.Broker
                 Console.WriteLine("Broker running");
                 Thread.Sleep(new TimeSpan(0, 1, 0));
             }
-
         }
 
         private static void ConfigureServices(IServiceCollection serviceCollection)
@@ -81,17 +78,16 @@ namespace MqttBrokerForNet.Broker
             serviceCollection.AddSingleton<IMqttLoadbalancingManager, MqttLoadbalancingManager>();
             serviceCollection.AddSingleton<IMqttRetainedMessageManager, MqttRetainedMessageManager>();
             serviceCollection.AddSingleton<IMqttSubscriptionManager, MqttSubscriptionManager>();
+            serviceCollection.AddSingleton<IMqttPublishingManager, MqttPublishingManager>();
+            serviceCollection.AddSingleton<IMqttSecurityManager, MqttSecurityManager>();
+            serviceCollection.AddSingleton<IMqttSessionManager, MqttSessionManager>();
+
             for (var i = 0; i < brokerOptions.NumberOfConnectionManagers; i++)
             {
                 serviceCollection.AddSingleton<IMqttConnectionManager, MqttConnectionManager>();
             }
 
             serviceCollection.AddSingleton(c => c.GetServices<IMqttConnectionManager>().ToArray());
-
-            // Managers - transient
-            serviceCollection.AddTransient<IMqttPublishingManager, MqttPublishingManager>();
-            serviceCollection.AddTransient<IMqttSecurityManager, MqttSecurityManager>();
-            serviceCollection.AddTransient<IMqttSessionManager, MqttSessionManager>();
 
             // Network - Singleton
             serviceCollection.AddSingleton<IMqttAsyncTcpSender, MqttAsyncTcpSender>();
@@ -100,12 +96,10 @@ namespace MqttBrokerForNet.Broker
 
             // Workers - Singleton
             serviceCollection.AddSingleton<IMqttRetainedMessageWorker, MqttRetainedMessageWorker>();
-
-            // Workers - Transient
-            serviceCollection.AddTransient<IMqttConnectionWorker, MqttConnectionWorker>();
-            serviceCollection.AddTransient<IMqttKeepAliveWorker, MqttKeepAliveWorker>();
-            serviceCollection.AddTransient<IMqttLoadbalancingWorker, MqttLoadbalancingWorker>();
-            serviceCollection.AddTransient<IMqttPublishingWorker, MqttPublishingWorker>();
+            serviceCollection.AddSingleton<IMqttConnectionWorker, MqttConnectionWorker>();
+            serviceCollection.AddSingleton<IMqttKeepAliveWorker, MqttKeepAliveWorker>();
+            serviceCollection.AddSingleton<IMqttLoadbalancingWorker, MqttLoadbalancingWorker>();
+            serviceCollection.AddSingleton<IMqttPublishingWorker, MqttPublishingWorker>();
         }
     }
 }
